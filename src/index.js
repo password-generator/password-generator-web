2acc15af59407b3ce699db21863350d5140706a2
Mikael-R <mikarg9@gmail.com>
2020-06-19 19:55:03 -0300

    feat: 🎸 Added serviceWorker no PWA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+serviceWorker.register()
