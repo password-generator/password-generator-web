a19a47f971c6cd253ce1d42bba31f786ac196098
Mateus Henrique <59037640+mateushnsoares@users.noreply.github.com>
2020-08-13 16:49:36 -0300

    chore: add eslint full configuration (#14)
    * ci: add commitlint/commitizen and Github Actions CI Workflow
    
    add commitlint/commitizen to automatize the commit message contruction and ci workflow to non pass a
    pull request with invalids commits
    
    BREAKING CHANGE: Not add emojis in commit messsage now
    
    * chore: add eslint full configuration
    
    Add eslint and lint-staged config to automatize the code style
    
    Co-authored-by: MateusSoares895 <mateushenreiquensoares@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.querySelector('#root')
-)
+  document.querySelector('#root'),
+);
 
-serviceWorker.register()
+serviceWorker.register();
