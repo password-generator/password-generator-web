239dc459b59cffce565952a1b273893db411ed41
wesleyburlani <wesleyburlani@gmail.com>
2020-08-25 10:00:12 -0300

    fix: cypress security issues to avoid errors on application default value changing.

--- cypress/integration/generate_password.spec.js
+++ cypress/integration/generate_password.spec.js
@@ -1,28 +1,33 @@
-describe("Generate password tests", () => {
-    it("should generate only numbers", () => {
-        cy.visit("/")
-        cy.get('input[type=checkbox]').uncheck()
-        cy.get('#numbers').check()
-        cy.get('#generatePasswordButton').click()
-        cy.get('#generatePasswordButton')
-        cy.get('#resultSpan').invoke('text').should('match', /[0-9]{6}/)
-    }) 
+/* eslint-disable no-undef */
 
-    it("should generate only lowercase letters", () => {
-        cy.visit("/")
-        cy.get('input[type=checkbox]').uncheck()
-        cy.get('#lowercase').check()
-        cy.get('#generatePasswordButton').click()
-        cy.get('#generatePasswordButton')
-        cy.get('#resultSpan').invoke('text').should('match', /[a-z]{6}/)
-    })
-    
-    it("should generate only uppercase letters", () => {
-        cy.visit("/")
-        cy.get('input[type=checkbox]').uncheck()
-        cy.get('#uppercase').check()
-        cy.get('#generatePasswordButton').click()
-        cy.get('#generatePasswordButton')
-        cy.get('#resultSpan').invoke('text').should('match', /[A-Z]{6}/)
-    })
-})
+describe('Generate password tests', () => {
+  it('should generate only numbers', () => {
+    cy.visit('/');
+    cy.get('#passwordLengthInput').type('{selectall}7');
+    cy.get('input[type=checkbox]').uncheck();
+    cy.get('#numbers').check();
+    cy.get('#generatePasswordButton').click();
+    cy.get('#generatePasswordButton');
+    cy.get('#resultSpan').invoke('text').should('match', /^[0-9]{7}$/);
+  });
+
+  it('should generate only lowercase letters', () => {
+    cy.visit('/');
+    cy.get('#passwordLengthInput').type('{selectall}7');
+    cy.get('input[type=checkbox]').uncheck();
+    cy.get('#lowercase').check();
+    cy.get('#generatePasswordButton').click();
+    cy.get('#generatePasswordButton');
+    cy.get('#resultSpan').invoke('text').should('match', /^[a-z]{7}$/);
+  });
+
+  it('should generate only uppercase letters', () => {
+    cy.visit('/');
+    cy.get('#passwordLengthInput').type('{selectall}7');
+    cy.get('input[type=checkbox]').uncheck();
+    cy.get('#uppercase').check();
+    cy.get('#generatePasswordButton').click();
+    cy.get('#generatePasswordButton');
+    cy.get('#resultSpan').invoke('text').should('match', /^[A-Z]{7}$/);
+  });
+});
